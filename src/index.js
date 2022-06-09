68493c68f7435b08f2e1de8c256b172211b4a122
anna <anjankow@protonmail.com>
2022-06-09 15:46:11 -0400

    add login page

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import authentication from './b2c';
 
-const root  = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+authentication.initialize();
+
+authentication.run(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
