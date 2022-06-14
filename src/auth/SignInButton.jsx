c9fbb17e1cda306b1e234e40bc63d31c7c0a630e
anna <anjankow@protonmail.com>
2022-06-14 16:08:47 -0400

    fix usused vars warnings

--- src/auth/SignInButton.jsx
+++ src/auth/SignInButton.jsx
@@ -1,20 +1,13 @@
 // source: https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/b2c-sample
-import { useState } from "react";
 import { useMsal } from "@azure/msal-react";
 import Button from "@mui/material/Button";
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 
 import { loginRequest } from "./authConfig.js";
 
 export const SignInButton = () => {
     const { instance } = useMsal();
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-
     const handleLogin = (loginType) => {
-        setAnchorEl(null);
 
         if (loginType === "popup") {
             instance.loginPopup(loginRequest).catch(e => {
@@ -33,24 +26,6 @@ export const SignInButton = () => {
             >
                 Login
             </Button>
-            {/* <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={open}
-                onClose={() => setAnchorEl(null)}
-            >
-                <MenuItem onClick={() => handleLogin("popup")} key="loginPopup">Sign in using Popup</MenuItem>
-                <MenuItem onClick={() => handleLogin("redirect")} key="loginRedirect">Sign in using Redirect</MenuItem>
-            </Menu> */}
         </div>
     )
 };
